5b144fcec3bb5f041e6d4ea952b0af87c01617ca
Daniel Chan <chanhoyin0305@gmail.com>
2025-06-18 16:49:10 +0800

    test: just testing

---
+++ testing.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include "library/modint.h"
+
+using namespace std;
+
+typedef long long ll;
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+void solve(){
+    modint<int, 1000000007> a(3); // Replace 1000000007 with the correct modulus if needed
+
+    cout << a << endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int NO_OF_TESTCASES = 1;
+    //cin >> NO_OF_TESTCASES;
+
+    for (int TESTCASE = 1; TESTCASE <= NO_OF_TESTCASES; TESTCASE++){
+        solve();
+    }
+}
