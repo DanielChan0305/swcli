e85ca46c012d3cac8a6aedbcd71d63b63beefb1b
Daniel Chan <chanhoyin0305@gmail.com>
2025-06-18 16:48:02 +0800

    feat(starterTemplate.cpp): Add testcase counter and defines

--- library/starterTemplate.cpp
+++ library/starterTemplate.cpp
@@ -1,6 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+void solve(){
+
+}
+
 int main(){
-    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int NO_OF_TESTCASES = 1;
+    cin >> NO_OF_TESTCASES;
+
+    for (int TESTCASE = 1; TESTCASE <= NO_OF_TESTCASES; TESTCASE++){
+        solve();
+    }
 }
