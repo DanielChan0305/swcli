b54fe0ba660d419a54f99a73f821cb3d7dd76549
Daniel Chan <chanhoyin0305@gmail.com>
2025-06-18 18:07:03 +0800

    feat(modint.h): add custom operators "+", "-", "*"

--- library/modint.h
+++ library/modint.h
@@ -4,29 +4,76 @@
 template <typename T, T MOD> class modint{
     private:
         T x;
+        T mod (T value){
+            
+            return (value % MOD + MOD) % MOD;
+        }
+
     public: 
-        modint(T value) : x(value % MOD) {}
+
+        modint(T value) : x(mod(value)) {}
 
         friend std::ostream &operator<<(std::ostream& os, const modint& m) {
             return os << m.x;
         }
 
-        friend std::istream &operator>>(std::istream& os, const modint& m){
-            return os >> m.x;
+        friend std::istream &operator>>(std::istream& is, const modint& m){
+            return is >> m.x;
+        }
+
+        // + 
+        modint operator+(const modint& m){
+            return modint(this->x + m.x);
+        }
+
+        modint &operator+=(const modint& m){
+            *this = *this + m;
+            return *this;
+        }
+
+        modint &operator++(){
+            *this = modint(this->x + 1);
+            return *this;
+        }
+
+        modint operator++(int){
+            modint cpy(*this);
+            ++*this;
+            return cpy;
+        }
+
+        // -
+        modint operator-(const modint& m){
+            return modint(this->x - m.x);
+        }
+
+        modint &operator-=(const modint& m){
+            *this = *this - m;
+            return *this;
+        }
+
+        modint &operator--(){
+            *this = modint(this->x - 1);
+            return *this;
+        }
+
+        modint operator--(int){
+            modint cpy(*this);
+            --*this;
+            return cpy;
+        }
+
+        // *
+        modint operator*(const modint& m){
+            return modint(this->x * m.x);
+        }
+
+        modint &operator*=(const modint& m){
+            *this = *this * m;
+            return *this;
         }
 };
 
-template <typename T> T cadd(T a, T b)
-{
-    return (a + b);
-}
 
-template <typename T> T cminus(T a, T b){
-    return (a - b);
-}
-
-template <typename T> T cmultiply(T a, T b){
-    return (a * b);
-}
 
 #endif
