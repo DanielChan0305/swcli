aa17c1c02edcf8b531326c777ba3b29f3ad3a710
Daniel Chan <chanhoyin0305@gmail.com>
2025-06-18 12:07:30 +0800

    create sanitization for entering filename in create.go

--- cmd/create.go
+++ cmd/create.go
@@ -2,10 +2,8 @@ package cmd
 
 import (
 	"errors"
-	"fmt"
-	"path/filepath"
-	"regexp"
 
+	"github.com/DanielChan0305/swcli/helper"
 	"github.com/spf13/cobra"
 )
 
@@ -15,30 +13,6 @@ var (
 
 func init() {
 	rootCmd.AddCommand(createCmd)
-}
-
-func isFilenameValid(filename string) (bool, error) {
-	if filename == "" {
-		return false, errors.New("filename can't be empty")
-	}
-
-	// check if filename matches the required regex (allowing relative and absolute paths, alphanumerics, underscores, hyphens, and dots)
-	matched, err := regexp.MatchString(`^([a-zA-Z0-9_\-./]+)$`, filename)
-	if err != nil {
-		return false, fmt.Errorf("error matching filename regex: %w", err)
-	}
-	if !matched {
-		return false, errors.New("filename contains invalid characters or format")
-	}
-
-	// check whether it is a cpp file
-	ext := filepath.Ext(filename)
-
-	if ext != ".cpp" {
-		return false, errors.New("only .cpp files can be created")
-	}
-
-	return true, nil
 }
 
 // createCmd creates a new file based on the starter template
@@ -51,7 +25,12 @@ var createCmd = &cobra.Command{
 			return errors.New("filename can't be empty")
 		}
 
-		if _, err := isFilenameValid(args[0]); err != nil {
+		filename := args[0]
+		if !helper.IsCpp(filename) {
+			return errors.New("only .cpp files can be created")
+		}
+
+		if _, err := helper.IsFilenameValid(filename); err != nil {
 			return err
 		}
 
