778e694d2e212088fac06944b67dc2a98a20594a
Daniel Chan <chanhoyin0305@gmail.com>
2025-06-20 15:33:19 +0800

    feat(get.go): allow copying of template code into clipboard

--- cmd/template/get.go
+++ cmd/template/get.go
@@ -1,12 +1,61 @@
 package template
 
-import "github.com/spf13/cobra"
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/DanielChan0305/swcli/helper"
+	"github.com/atotto/clipboard"
+	"github.com/spf13/cobra"
+)
 
 // getCmd copys the desired template into clipboard
 var getCmd = &cobra.Command{
 	Use:   "get [template name]",
 	Short: "Copys the desired template into clipboard",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		return cmd.Help()
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("template name can't be empty")
+		}
+
+		templateName := args[0]
+		if _, err := helper.IsFilenameValid(templateName); err != nil {
+			return err
+		}
+
+		return nil
 	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		templateName := args[0]
+		templateFolder, err := helper.JsonGetFieldString(configTemplatePath, "templateFolder")
+
+		if err != nil {
+			return err
+		}
+
+		templatePath := templateFolder + "/" + templateName + ".h"
+		//fmt.Println(templatePath)
+		if !helper.IsFileExist(templatePath) {
+			return fmt.Errorf("can't find template: %s", templateName)
+		}
+
+		fmt.Printf("✅ Found template: %s\n", helper.TrimExt(templatePath))
+		templateContent, err := os.ReadFile(templatePath)
+
+		if err != nil {
+			return err
+		}
+
+		err = clipboard.WriteAll(string(templateContent))
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("✅ Copied %s into clipboard\n", helper.TrimExt(templatePath))
+
+		return nil
+	},
+
+	SilenceUsage: true,
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/DanielChan0305/swcli
 go 1.24.2
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/briandowns/spinner v1.23.2 // indirect
 	github.com/fatih/color v1.7.0 // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
